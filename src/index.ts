eae8cad724a5b6d7d956b3e48b8e8e66069575fe
Kumari Anushka <anushka.ak14@gmail.com>
2023-02-16 13:43:57 +0530

    more

--- src/index.ts
+++ src/index.ts
@@ -56,4 +56,51 @@ function kgToLbs(weight: number | string): number {
 kgToLbs(10);
 kgToLbs('10kg');
 
-// 
+// Intersection Types
+type Draggable = {
+  drag: () => void
+}
+
+type Resizable = {
+  resize: () => void
+}
+
+type UIWidget = Draggable & Resizable;
+let textBox: UIWidget = {
+  drag: () => { },
+  resize: () => { }
+}
+
+// Literal (exact,specific)
+type Quantity = 50 | 100;
+let quantity: Quantity = 100;
+
+type Metric = 'cm' | 'inch';
+
+// Nullable Types
+function greet(name: string | null | undefined) {
+  if (name)
+    console.log(name.toUpperCase());
+  else
+    console.log("Hola")
+}
+greet(null);
+
+// Optional Chaining
+type Customer = {
+  birthday: Date
+}
+
+function getCustomer(id: number): Customer | null {
+  return id === 0 ? null : { birthday: new Date() }
+}
+
+let customer = getCustomer(1);
+// Optional property access operator
+console.log(customer?.birthday?.getFullYear());
+
+// customers?.[0]
+
+// Optional call
+let log: any = null;
+log?.('a');
